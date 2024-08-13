9b7fad635ea9838b02ad010f059d5635beb592f3
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-13 11:30:11 +0900

    feat: 기물 배치

--- game_chess/javascript/main.js
+++ game_chess/javascript/main.js
@@ -3,21 +3,42 @@ const cnt = 8;
 document.addEventListener("DOMContentLoaded", function () {
   const chessboard = document.getElementById("chessboard");
   const boardFrag = document.createDocumentFragment();
+ 
+  const initialBoardSetup = [
+    'rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook',
+    'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn',
+    'rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook',
+  ]
 
   for (let i = 0; i < Math.pow(cnt, 2); i++) {
+    const row = Math.floor(i / cnt);
+    const col = i % cnt;
+    
     const square = document.createElement("div");
-    square.className = `square ${calBoardColor(i)}`;
+    square.className = `square ${calBoardColor(row, col)}`;
     square.setAttribute("name", calSquareSymbol(i));
+
+    const piece = initialBoardSetup[i];
+    if (piece) {
+      const img = document.createElement("img");
+      img.src = `/game_chess/javascript/images/${row < 2 ? 'white' : 'black'}_${piece}.svg`;
+      img.alt = piece;
+      square.appendChild(img);
+    }
+
     boardFrag.appendChild(square);
   }
+
 
   chessboard.appendChild(boardFrag);
 });
 
-const calBoardColor = (i) => {
-  const row = Math.floor(i / cnt);
-  const col = i % cnt;
-
+const calBoardColor = (row, col) => {
   if ((row + col) % 2 == 0) return "white";
   else return "black";
 };
