063367a135062974a2b13e675d4c34dd3ad872db
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-13 11:30:34 +0900

    refactor: 함수정리

--- game_chess/javascript/main.js
+++ game_chess/javascript/main.js
@@ -18,10 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < Math.pow(cnt, 2); i++) {
     const row = Math.floor(i / cnt);
     const col = i % cnt;
+    const rank = cnt - Math.floor(i / cnt); // 행 숫자
+    const file = String.fromCharCode(97 + (i % cnt)); // 열 알파벳
     
     const square = document.createElement("div");
-    square.className = `square ${calBoardColor(row, col)}`;
-    square.setAttribute("name", calSquareSymbol(i));
+    square.className = `square ${(row + col) % 2 == 0 ? 'white' : 'black'}`;
+    square.setAttribute("name", `${file}${rank}`);
 
     const piece = initialBoardSetup[i];
     if (piece) {
@@ -34,18 +36,5 @@ document.addEventListener("DOMContentLoaded", function () {
     boardFrag.appendChild(square);
   }
 
-
   chessboard.appendChild(boardFrag);
 });
-
-const calBoardColor = (row, col) => {
-  if ((row + col) % 2 == 0) return "white";
-  else return "black";
-};
-
-const calSquareSymbol = (i) => {
-  const rank = cnt - Math.floor(i / cnt);
-  const file = String.fromCharCode(97 + (i % cnt));
-
-  return `${file}${rank}`;
-};
