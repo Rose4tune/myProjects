73f6c8b984b1fa0f31ae5b0dcca6cb04c6288ba9
Lee Ye Seo <rosefor2ne@gmail.com>
2024-08-13 14:08:25 +0900

    refactor: 기물 선택 및 이동기능 구현

--- game_chess/javascript/main.js
+++ game_chess/javascript/main.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
     'rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook',
   ]
 
+  let selectedPiece = null;
+  let selectedSquare = null;
+
+  const removeSelected = () => {
+    selectedSquare.classList.remove("selected");
+    selectedPiece = null;
+    selectedSquare = null;
+  };
   for (let i = 0; i < Math.pow(cnt, 2); i++) {
     const row = Math.floor(i / cnt);
     const col = i % cnt;
@@ -22,63 +30,42 @@ document.addEventListener("DOMContentLoaded", function () {
     const file = String.fromCharCode(97 + (i % cnt)); // 열 알파벳
     
     const square = document.createElement("div");
-    square.className = `square ${(row + col) % 2 == 0 ? 'white' : 'black'}`;
+    square.className = `square ${(row + col) % 2 == 0 ? "white" : "black"}`;
     square.setAttribute("name", `${file}${rank}`);
 
     const piece = initialBoardSetup[i];
     if (piece) {
       const img = document.createElement("img");
-      const color = row < 2 ? 'white' : 'black';
+      const color = row > 2 ? "white" : "black";
       img.src = `/game_chess/javascript/images/${color}_${piece}.svg`;
       img.alt = piece;
       img.setAttribute("color", color);
       square.appendChild(img);
     }
 
+    square.addEventListener("click", function () {
+      if (selectedPiece) {
+        if (selectedSquare === square) {
+          removeSelected();
+
+        } else {
+          const targetSquare = square;
+
+          if (!targetSquare.hasChildNodes()) {
+            targetSquare.appendChild(selectedPiece);
+            removeSelected();
+          }
+        }
+        
+      } else if (square.hasChildNodes()) {
+        selectedPiece = square.firstChild;
+        selectedSquare = square;
+        selectedSquare.classList.add("selected");
+      }
+    });
+
     boardFrag.appendChild(square);
   }
 
   chessboard.appendChild(boardFrag);
 });
-
-
-let selectedPiece = null;
-let selectedSquare = null;
-
-
-document.addEventListener("click", (e) => {
-  if (!e.target.classList.contains('square')) return
-
-  const targetSquare = e.target;
-
-  const removeSelected = () => {
-    selectedSquare.classList.remove("selected");
-    selectedPiece = null;
-    selectedSquare = null;
-  };
-
-  if (!selectedPiece && targetSquare.firstChild) {
-    selectedPiece = e.target.firstChild;
-    selectedSquare = e.target;
-    selectedSquare.classList.add("selected");
-  }
-
-  if (selectedPiece) {
-    if (targetSquare !== selectedSquare) {
-      if (!targetSquare.hasChildNodes()) {
-        targetSquare.appendChild(selectedPiece)
-        removeSelected();
-
-      } else {
-        const selectedColor = selectedPiece.getAttribute("color");
-        const targetColor = targetSquare.firstChild.getAttribute("color");
-
-        if (selectedColor !== targetColor) {
-          targetSquare.firstChild.remove();
-          targetSquare.appendChild(selectedPiece)
-          removeSelected();
-        }
-      }
-    }
-  }
-})
